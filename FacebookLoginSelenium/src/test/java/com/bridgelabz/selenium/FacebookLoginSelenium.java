ea41d1e2a83f34744231281485dbecf16009f84f
kuldeepkumar17feb@gmail.com <kuldeepkumar17feb@gmail.com>
2024-01-01 00:42:58 +0530

    Added[Kuldeep]:Welcome to Selenium

---
+++ FacebookLoginSelenium/src/test/java/com/bridgelabz/selenium/FacebookLoginSelenium.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FacebookLoginSelenium {
+    public static void main(String[] args) {
+        System.out.println("Welcome to selenium");
+    }
+}
