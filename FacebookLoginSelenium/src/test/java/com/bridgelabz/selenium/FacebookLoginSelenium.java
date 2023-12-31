118b9b8798272985f3abaf98c6075b8c492c0827
kuldeepkumar17feb@gmail.com <kuldeepkumar17feb@gmail.com>
2024-01-01 00:47:58 +0530

    Added[Kuldeep]:Facebook Login page assignment

--- FacebookLoginSelenium/src/test/java/com/bridgelabz/selenium/FacebookLoginSelenium.java
+++ FacebookLoginSelenium/src/test/java/com/bridgelabz/selenium/FacebookLoginSelenium.java
@@ -7,7 +7,30 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class FacebookLoginSelenium {
-    public static void main(String[] args) {
-        System.out.println("Welcome to selenium");
+    public WebDriver driver;
+
+    @Test
+    public void fbLogin() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/login/");
+        Thread.sleep(2000);
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        WebElement element = driver.findElement(By.name("email"));
+        element.sendKeys("Kuldeep@gmail.com");
+        Thread.sleep(2000);
+        driver.findElement(By.id("pass")).sendKeys("kul@123");
+//        Thread.sleep(2000);
+//        driver.findElement(By.cssSelector("#loginbutton")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.linkText("Forgotten account?")).click();
+        Thread.sleep(2000);
+//        driver.findElement(By.linkText("Yes, Continue")).click();
+        driver.findElement(By.xpath("//input[@id='identify_email']")).sendKeys("kuldeepkumar17feb@gmail.com");
+        Thread.sleep(2000);
+        driver.findElement(By.id("did_submit")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//font[contains(text(),'find out')]")).click();
+        Thread.sleep(2000);
     }
 }
