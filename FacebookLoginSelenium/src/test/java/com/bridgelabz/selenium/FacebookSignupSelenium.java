2ea0a05706d7a97ff6cb69a6cbbdfd3854f936fb
kuldeepkumar17feb@gmail.com <kuldeepkumar17feb@gmail.com>
2024-01-01 02:02:55 +0530

    Added[Kuldeep]:Facebook Signup task selenium

---
+++ FacebookLoginSelenium/src/test/java/com/bridgelabz/selenium/FacebookSignupSelenium.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FacebookSignupSelenium {
+    public WebDriver driver;
+
+    @Test
+    public void fbSignupApplication() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.manage().window().maximize();
+        driver.findElement(By.linkText("Create new account")).click();
+        Thread.sleep(4000);
+        driver.findElement(By.name("firstname")).sendKeys("Kuldeep");
+        Thread.sleep(2000);
+        driver.findElement(By.name("lastname")).sendKeys("Sharma");
+        Thread.sleep(2000);
+        driver.findElement(By.name("reg_email__")).sendKeys("kuldeepkumar93203@gmail.com");
+        Thread.sleep(1000);
+        driver.findElement(By.name("reg_email_confirmation__")).sendKeys("kuldeepkumar93203@gmail.com");
+        Thread.sleep(1000);
+        driver.findElement(By.name("reg_passwd__")).sendKeys("admin@123");
+        Thread.sleep(1000);
+        driver.findElement(By.name("birthday_day")).sendKeys("25");
+        Thread.sleep(1000);
+        driver.findElement(By.name("birthday_month")).sendKeys("April");
+        Thread.sleep(1000);
+        driver.findElement(By.name("birthday_year")).sendKeys("2012");
+        Thread.sleep(1000);
+//        driver.findElement(By.xpath ("//font[contains(text(),'male')]"));
+//        Thread.sleep(1000);
+        driver.findElement(By.className("_58mt")).click();
+        Thread.sleep(2000);
+//           driver.findElement(By.name("preferred_pronoun")).click();
+        driver.findElement(By.xpath("//button[@id='u_2_s_fX']")).click();
+        Thread.sleep(1000);
+    }
+}
