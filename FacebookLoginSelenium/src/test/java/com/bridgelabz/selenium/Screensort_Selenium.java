c3c4b53b4efdbac6c2980eb9d31953d02ac07ce4
kuldeepkumar17feb@gmail.com <kuldeepkumar17feb@gmail.com>
2024-01-01 02:23:38 +0530

    Added[Kuldeep]:Screensort_selenium

---
+++ FacebookLoginSelenium/src/test/java/com/bridgelabz/selenium/Screensort_Selenium.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.selenium;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+
+public class Screensort_Selenium {
+    public WebDriver driver;
+    @Test
+    public void screenSort() throws InterruptedException, AWTException, IOException {
+        driver =new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        Robot robot=new Robot();
+        robot.keyPress(KeyEvent.VK_K);
+        robot.keyRelease(KeyEvent.VK_K);
+        TakesScreenshot takesScreenshot=(TakesScreenshot)driver;
+        File file1=takesScreenshot.getScreenshotAs(OutputType.FILE);
+        File file2=new File("C:\\Users\\Admin\\Desktop\\SeleniumProjects\\screesort\\fb_Login.jpg");
+        FileHandler.copy(file1, file2);
+
+    }
+}
