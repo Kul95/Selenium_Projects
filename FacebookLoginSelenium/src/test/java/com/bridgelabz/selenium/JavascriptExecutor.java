5b714e20dd92a236f1746a6eff6a9c862de1fb6e
kuldeepkumar17feb@gmail.com <kuldeepkumar17feb@gmail.com>
2024-01-01 22:34:36 +0530

    Added[Kuldeep]:Simple alert1

---
+++ FacebookLoginSelenium/src/test/java/com/bridgelabz/selenium/JavascriptExecutor.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.selenium;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class JavascriptExecutor {
+    public WebDriver driver;
+    @Test
+    public void simple_alert() throws InterruptedException {
+        driver=new ChromeDriver();
+        driver.get("https://demoqa.com/alerts");
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+        driver.findElement(By.id("alertButton")).click();
+        Alert alert=driver.switchTo().alert();
+        Thread.sleep(2000);
+        System.out.println(alert.getText());
+        Thread.sleep(3000);
+        alert.accept();
+        Thread.sleep(2000);
+    }
+}
